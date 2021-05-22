dc378b384e22b80e594c9572b89f8571b095737e
Steven <sjecmen@gmail.com>
2021-05-21 20:21:46 -0400

    Update split_samples

--- get_split_samples.py
+++ get_split_samples.py
@@ -6,7 +6,7 @@ from load_sims import *
 Runs the main experiment by saving the value of several trials of random split against the optimal
 '''
 
-fname = 'preflib3'
+fname = 'query'
 
 S, M = load_sims('datasets/' + fname)
 revs, paps = S.shape
@@ -15,7 +15,7 @@ revs, paps = S.shape
 papload = 2
 revload = 6
 T = 10
-cs = [0.1, 0.3, 0.5, 1]
+cs = [0.1, 0.3, 0.5]
 opt_data = {c:[] for c in cs}
 split_data = {c:[] for c in cs}
 
@@ -25,8 +25,9 @@ for frac in cs:
     nreviews = (paps + num_second_round_paps) * papload
 
     for t in range(T):
+        print(frac, t)
         v, v_opt = split_assignment(S, M, revload, papload, frac) # experimental results
-        opt_data[frac].append(v/nreviews)
-        split_data[frac].append(v_opt/nreviews)
+        opt_data[frac].append(v_opt/nreviews)
+        split_data[frac].append(v/nreviews)
 
 np.savez('split_samples_'+fname+'.npz', opt_samples=opt_data, split_samples=split_data, papload=papload, revload=revload)
